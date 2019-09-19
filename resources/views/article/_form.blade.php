6c9556517d0b703a389b8dc8d43dea51e8cd712a
Hon BUI <buivanhon@gmail.com>
2019-09-19 03:07:30 +0200

    first commit

--- resources/views/article/_form.blade.php
+++ resources/views/article/_form.blade.php
@@ -1,12 +1,12 @@
 <div class="form-group">
-    <input type="text" name="title" id="title" placeholder="Title" class="form-control @error('title') is-invalid @enderror" value="{{ $article->title }}" required="required" />
+    <input type="text" name="title" id="title" placeholder="Title" class="form-control @error('title') is-invalid @enderror" value="{{ old('title', $article->title) }}" required="required" />
     @error('title')
         <div class="alert alert-danger">{{ $message }}</div>
     @enderror
 </div>
 
 <div class="form-group">
-    <textarea name="content" id="content" cols="30" rows="10" placeholder="Content" class="form-control @error('content') is-invalid @enderror" required="required">{{ $article->content }}</textarea>
+    <textarea name="content" id="content" cols="30" rows="10" placeholder="Content" class="form-control @error('content') is-invalid @enderror" required="required">{{ old('content', $article->content) }}</textarea>
     @error('content')
         <div class="alert alert-danger">{{ $message }}</div>
     @enderror
